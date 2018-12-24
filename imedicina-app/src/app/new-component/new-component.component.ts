a8076b130f2af4a53938f5dff560b82f329ba4d8
barbara-aguilar <bahlutz.design@gmail.com>
2018-12-24 11:45:50 -0200

    creat a angular project

---
+++ imedicina-app/src/app/new-component/new-component.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-new-component',
+  templateUrl: './new-component.component.html',
+  styleUrls: ['./new-component.component.css']
+})
+export class NewComponentComponent  {
+
+
+}
